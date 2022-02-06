6ed947e0c549e9375db0b932802fa6a9397610a7
Mustafa Dinler <dinlermstf@gmail.com>
2022-02-06 16:00:35 +0100

    chore: init spring project (#1)

---
+++ src/main/java/com/group12/ciserver/CiServerApplication.java
@@ -0,0 +1,13 @@
+package com.group12.ciserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group12/ciserver/CiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group12.ciserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
