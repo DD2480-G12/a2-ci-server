a7d2229293d8527f8d4d9ba31faa572494f5bfb2
Mustafa Dinler <dinlermstf@gmail.com>
2022-02-07 07:50:49 +0100

    feat: add push event payload (#10)
    Added necessary properties for:
    - cloning git repo and checking out a branch
    - updating commit status

---
+++ src/main/java/com/group12/ciserver/model/github/Repository.java
@@ -0,0 +1,13 @@
+package com.group12.ciserver.model.github;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class Repository {
+
+    private String name;
+
+    @JsonProperty("clone_url")
+    private String cloneUrl;
+}
