b6cc74798a481eb73e4e64135d23d6d45cf0e997
Mustafa Dinler <dinlermstf@gmail.com>
2022-02-08 20:58:47 +0100

    refactor: remove GithubClient from EventController (#17)
    It looks like it was forgotten there after local testing.

--- src/main/java/com/group12/ciserver/controller/EventController.java
+++ src/main/java/com/group12/ciserver/controller/EventController.java
@@ -1,10 +1,7 @@
 package com.group12.ciserver.controller;
 
-import com.group12.ciserver.GithubClient;
-import com.group12.ciserver.model.github.CommitState;
 import com.group12.ciserver.model.github.PushEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class EventController {
-
-    @Autowired
-    private GithubClient githubClient;
 
     @PostMapping("/push-events")
     public ResponseEntity<Void> pushEvent(@RequestBody PushEvent pushEvent) {
