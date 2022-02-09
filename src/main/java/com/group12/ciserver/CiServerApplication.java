89d833e64473ff33b17abe1c6f0c9dd8f598ed75
nichujie <ncj19991213@126.com>
2022-02-09 23:24:46 +0100

    test: persistence (server restart) & build success

--- src/main/java/com/group12/ciserver/CiServerApplication.java
+++ src/main/java/com/group12/ciserver/CiServerApplication.java
@@ -1,6 +1,7 @@
 package com.group12.ciserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
