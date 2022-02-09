0eedc189e5570b5186d4b9328f977ea3e6f3a0ea
nichujie <ncj19991213@126.com>
2022-02-09 22:59:14 +0100

    test: build failure test

--- src/test/java/com/group12/ciserver/CiServerApplicationTests.java
+++ src/test/java/com/group12/ciserver/CiServerApplicationTests.java
@@ -3,11 +3,17 @@ package com.group12.ciserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 
 @SpringBootTest
 class CiServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test void failureTest() {
+		fail();
 	}
 }
