29ed47b6155ea52a7c25f73bcb734aa3d7c5cd7b
nichujie <ncj19991213@126.com>
2022-02-11 10:52:35 +0100

    test: compile fail

--- src/main/java/com/group12/ciserver/CiServerApplication.java
+++ src/main/java/com/group12/ciserver/CiServerApplication.java
@@ -2,7 +2,6 @@ package com.group12.ciserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
